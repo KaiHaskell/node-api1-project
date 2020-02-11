498ffa9e7834951ff3b7ec9ca54b159599b5cc19
Kai <31229627+KaiHaskell@users.noreply.github.com>
2020-02-11 11:35:15 -0500

    Setup complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // implement your API here
+const express = require("express");
+const Data = require("./data/db");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/api/users", (req, res) => {
+  Data.find()
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ errorMessage: "Oh no, couldn't make the GET request" });
+    });
+});
